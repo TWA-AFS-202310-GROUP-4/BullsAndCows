5ecd525841b74389de4736b976912bd944265aae
Jingbo Yu <Jyu20@slb.com>
2023-11-01 17:35:15 +0800

    test: should return 0A2B when guess

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -37,10 +37,6 @@ namespace BullsAndCowsTest
             Assert.Equal(expectGuessResult, result);
         }
 
-        // 0A4B
-        // 2A2B
-        // 1A1B
-        // 0A2B
         [Theory]
         [InlineData("5678")]
         public void Should_return_0A0B_when_guess_given_0_correct_0_position_wrong(string guessString)
@@ -54,13 +50,55 @@ namespace BullsAndCowsTest
             var result = game.Guess(guessString);
             Assert.Equal(expectGuessResult, result);
         }
-        
+
         [Theory]
         [InlineData("4321")]
         public void Should_return_0A4B_when_guess_given_0_correct_4_position_wrong(string guessString)
         {
             string secret = "1234";
             var expectGuessResult = "0A4B";
+
+            mockedGenerator.Setup(x => x.GenerateSecret()).Returns(secret);
+
+            var game = new BullsAndCowsGame(mockedGenerator.Object);
+            var result = game.Guess(guessString);
+            Assert.Equal(expectGuessResult, result);
+        }
+
+        [Theory]
+        [InlineData("1243")]
+        public void Should_return_2A2B_when_guess_given_2_correct_2_position_wrong(string guessString)
+        {
+            string secret = "1234";
+            var expectGuessResult = "2A2B";
+
+            mockedGenerator.Setup(x => x.GenerateSecret()).Returns(secret);
+
+            var game = new BullsAndCowsGame(mockedGenerator.Object);
+            var result = game.Guess(guessString);
+            Assert.Equal(expectGuessResult, result);
+        }
+
+        [Theory]
+        [InlineData("1546")]
+        public void Should_return_1A1B_when_guess_given_1_correct_1_position_wrong(string guessString)
+        {
+            string secret = "1234";
+            var expectGuessResult = "1A1B";
+
+            mockedGenerator.Setup(x => x.GenerateSecret()).Returns(secret);
+
+            var game = new BullsAndCowsGame(mockedGenerator.Object);
+            var result = game.Guess(guessString);
+            Assert.Equal(expectGuessResult, result);
+        }
+
+        [Theory]
+        [InlineData("2156")]
+        public void Should_return_0A2B_when_guess_given_0_correct_2_position_wrong(string guessString)
+        {
+            string secret = "1234";
+            var expectGuessResult = "0A2B";
 
             mockedGenerator.Setup(x => x.GenerateSecret()).Returns(secret);
 
