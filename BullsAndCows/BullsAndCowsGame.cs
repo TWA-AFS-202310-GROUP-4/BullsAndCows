72186f39dbefd8d88bd1eaa61b19e6c909a60030
Dexin Wang <DWang36@slb.com>
2023-11-01 17:36:35 +0800

    feat(4): refact the guess

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -15,13 +15,13 @@ namespace BullsAndCows
         public string Guess(string guess)
         {
             string secret = secretGenerator.GenerateSecret();
-            if (secret.Equals(guess))
-            {
-                return "4A0B";
-            }
+            //if (secret.Equals(guess))
+            //{
+            //    return "4A0B";
+            //}
 
             int bulls = 0;
-
+            int cows = 0;
             for (int i = 0; i < secret.Length; i++)
             {
                 if (secret[i] == guess[i])
@@ -30,7 +30,15 @@ namespace BullsAndCows
                 }
             }
 
-            return $"{bulls}A0B";
+            for (int i = 0; i < secret.Length; i++)
+            {
+                if (secret.IndexOf(guess[i]) >= 0 && secret.IndexOf(guess[i]) != i)
+                {
+                    cows++;
+                }
+            }
+
+            return $"{bulls}A{cows}B";
         }
     }
 }
