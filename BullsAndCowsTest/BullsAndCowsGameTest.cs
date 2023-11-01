d8cd916b067602663be7d2c9d3488bc921f6a67f
Xinwen Xiang <XXiang2@slb.com>
2023-11-01 17:17:06 +0800

    test: remove Should_create_BullsAndCowsGame test case

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -6,14 +6,6 @@ namespace BullsAndCowsTest
 {
     public class BullsAndCowsGameTest
     {
-        [Fact]
-        public void Should_create_BullsAndCowsGame()
-        {
-            var secretGenerator = new SecretGenerator();
-            var game = new BullsAndCowsGame(secretGenerator);
-            Assert.NotNull(game);
-            Assert.True(game.CanContinue);
-        }
 
         [Fact]
         public void Should_return_4A0B_when_guess_given_string_same_secretes()
