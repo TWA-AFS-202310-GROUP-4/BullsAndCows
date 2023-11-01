3ec160a794c6fb11e9864a5be3ee4dfac090196f
Ming Wang <MWang35@slb.com>
2023-11-01 17:02:20 +0800

    test: add 2A0B testcase

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -30,5 +30,20 @@ namespace BullsAndCowsTest
 
             Assert.Equal("4A0B", ret);
         }
+
+        [Fact]
+        public void Should_return_2A0B_when_guess_number_different_secret_number()
+        {
+            var guessNumber = "1536";
+            var secretNumber = "1234";
+            // var secretGenerator = new SecretGenerator();
+            var mockedSecretGenerator = new Mock<SecretGenerator>();
+            mockedSecretGenerator.Setup(generator => generator.GenerateSecret()).Returns(secretNumber);
+            var game = new BullsAndCowsGame(mockedSecretGenerator.Object);
+
+            var ret = game.Guess(guessNumber);
+
+            Assert.Equal("2A0B", ret);
+        }
     }
 }
