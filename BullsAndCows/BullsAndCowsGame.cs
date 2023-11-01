8ecf57e0edfc53892a5644b4256a4f5a8e4ea19d
Ming Wang <MWang35@slb.com>
2023-11-01 16:59:47 +0800

    feat: add guess function implement

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -5,16 +5,37 @@ namespace BullsAndCows
     public class BullsAndCowsGame
     {
         private readonly SecretGenerator secretGenerator;
+        private readonly string secretNumber;
+        private int attemp;
+
         public BullsAndCowsGame(SecretGenerator secretGenerator)
         {
             this.secretGenerator = secretGenerator;
+            this.secretNumber = secretGenerator.GenerateSecret();
         }
 
-        public bool CanContinue => true;
+        public bool CanContinue => attemp < 6;
 
         public string Guess(string guess)
         {
-            throw new NotImplementedException();
+            var bulls = 0;
+            var cows = 0;
+
+            for (var i = 0; i < guess.Length; i++)
+            {
+                if (guess[i] == secretNumber[i])
+                {
+                    bulls++;
+                }
+                else if (secretNumber.Contains(guess[i]))
+                {
+                    cows++;
+                }
+            }
+
+            attemp++;
+
+            return $"{bulls}A{cows}B";
         }
     }
 }
