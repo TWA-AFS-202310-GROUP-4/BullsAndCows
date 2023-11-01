26a4140d96ede26315a04540b6e6f08a48a697c1
Xinwen Xiang <XXiang2@slb.com>
2023-11-01 16:58:41 +0800

    test: add 4A0B test case for Guess

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -1,4 +1,5 @@
 using BullsAndCows;
+using Moq;
 using Xunit;
 
 namespace BullsAndCowsTest
@@ -12,6 +13,24 @@ namespace BullsAndCowsTest
             var game = new BullsAndCowsGame(secretGenerator);
             Assert.NotNull(game);
             Assert.True(game.CanContinue);
+        }
+
+        [Fact]
+        public void Should_return_4A0B_when_guess_given_string_same_secretes()
+        {
+            //given
+            var guessString = "1234";
+            var secret = "1234";
+
+            Mock<SecretGenerator> mockGenerator = new Mock<SecretGenerator>();
+            mockGenerator.Setup(m => m.GenerateSecret()).Returns(secret);
+            var game = new BullsAndCowsGame(mockGenerator.Object);
+
+            //when
+            var res = game.Guess(guessString);
+
+            //then
+            Assert.Equal("4A0B", res);
         }
     }
 }
