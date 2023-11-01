9201ddf693b4e49d643bd9ed9b2615f721bf6658
Diana Jing <DJing@slb.com>
2023-11-01 17:16:47 +0800

    test: complete the test case when position and digits are partially correct

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -31,11 +31,26 @@ namespace BullsAndCowsTest
             string result = game.Guess(guessNumber);
             //Then
             Assert.Equal("4A0B", result);
-
-
-
-
-
         }
+
+
+        [Fact]
+        public void Should_return_2A0B_when_guess_given_position_and_digits_are_partially_same()
+        {
+            //Given
+            string guessNumber = "1258";
+            string secret = "1234";
+
+            Mock<SecretGenerator> mockedSecretGenerator = new Mock<SecretGenerator>();
+            mockedSecretGenerator.Setup(generator => generator.GenerateSecret()).Returns(secret);
+
+            var game = new BullsAndCowsGame(mockedSecretGenerator.Object);
+
+            //When
+            string result = game.Guess(guessNumber);
+            //Then
+            Assert.Equal("2A0B", result);
+        }
+
     }
 }
