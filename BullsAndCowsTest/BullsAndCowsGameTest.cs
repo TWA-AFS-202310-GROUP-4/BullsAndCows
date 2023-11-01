3f688124f39a52a9e40dc2a7ff3a76622326473a
Jingbo Yu <Jyu20@slb.com>
2023-11-01 17:28:30 +0800

    test: should return 0A0B when guess

--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -7,7 +7,6 @@ namespace BullsAndCowsTest
 {
     public class BullsAndCowsGameTest
     {
-
         private Mock<SecretGenerator> mockedGenerator = new Mock<SecretGenerator>();
 
         [Fact]
@@ -24,12 +23,31 @@ namespace BullsAndCowsTest
             Assert.Equal(expectGuessResult, result);
         }
 
-        [Fact]
-        public void Should_return_2A0B_when_guess_given_2_correct_0_position_wrong()
+        [Theory]
+        [InlineData("1256")]
+        public void Should_return_2A0B_when_guess_given_2_correct_0_position_wrong(string guessString)
         {
             string secret = "1234";
-            var guessString = "1256";
             var expectGuessResult = "2A0B";
+
+            mockedGenerator.Setup(x => x.GenerateSecret()).Returns(secret);
+
+            var game = new BullsAndCowsGame(mockedGenerator.Object);
+            var result = game.Guess(guessString);
+            Assert.Equal(expectGuessResult, result);
+        }
+
+        // 0A0B
+        // 0A4B
+        // 2A2B
+        // 1A1B
+        // 0A2B
+        [Theory]
+        [InlineData("5678")]
+        public void Should_return_0A0B_when_guess_given_0_correct_0_position_wrong(string guessString)
+        {
+            string secret = "1234";
+            var expectGuessResult = "0A0B";
 
             mockedGenerator.Setup(x => x.GenerateSecret()).Returns(secret);
 
